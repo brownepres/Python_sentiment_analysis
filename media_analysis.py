1ecaaeb7bb50ccc89d372d4a7f48048d7f12610c
Epres Barnabás <barnabas.epres@gmail.com>
2024-04-16 10:38:16 +0200

    Final analyse and sample from the dataset, using both models

--- media_analysis.py
+++ media_analysis.py
@@ -1,86 +1,19 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import nltk
 import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import scipy.stats as stats
 from nltk.sentiment import SentimentIntensityAnalyzer
-from tqdm.notebook import tqdm
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
 
 plt.style.use("ggplot")
 
 df = pd.read_csv('../sentiment_analysis_data/Reviews.csv')
 
 sia = SentimentIntensityAnalyzer()
-
-n = 100
-k = 100
-
-column_names = ["Sample" + str(i +1) for i in range(n)]
-vader_text_sample = pd.DataFrame(columns=column_names)
-vader_rating_sample= pd.DataFrame(columns=column_names)
-vader_sample_column = pd.DataFrame(columns=["vader score"])
-np.random.seed(2002)
-for i in range(1):
-    sampled_element = df.sample(n, replace=True)
-    sampled_element_text = sampled_element['Text']
-    
-    for j in sampled_element_text:
-        vader_score = sia.polarity_scores(j)
-        vader_sample_column.loc[len(vader_sample_column)] = vader_score['compound']
-        
-    vader_text_sample.loc[i] = vader_sample_column["vader score"].values
-    vader_sample_column = vader_sample_column.drop(vader_sample_column.index, axis=0)
-    vader_rating_sample.loc[i] = sampled_element['Score'].values
-    
-vader_text_average = pd.DataFrame(np.mean(vader_text_sample.iloc[0:k, :], axis=0), columns=["vader_text_average"])
-vader_rating_average = pd.DataFrame(np.mean(vader_rating_sample.iloc[0:k, :], axis=0), columns=["vader_rating_average"])
-
-# Merge the DataFrames
-vader_result_final = vader_text_average.merge(vader_rating_average, how="left", left_index=True, right_index=True)
-   
-melted_data = vader_result_final.head(20).reset_index().melt(id_vars='index')
-
-# Create a figure and axes
-fig, ax1 = plt.subplots(figsize=(10, 6))
-
-# Plot the text average as dots on the first y-axis
-sns.scatterplot(data=melted_data[melted_data['variable'] == 'vader_text_average'],
-                x='index', y='value', color='b', ax=ax1, label='Text Average')
-
-# Create a second y-axis for ratings
-ax2 = ax1.twinx()
-# Plot the rating averages as dots on the second y-axis
-sns.scatterplot(data=melted_data[melted_data['variable'] == 'vader_rating_average'],
-                x='index', y='value', color='r', ax=ax2, label='Rating Average')
-
-# Set y-axis limits according to the data ranges
-ax1.set_ylim(0, 1)
-ax2.set_ylim(1, 5)
-
-# Set labels and titles
-ax1.set_xlabel('Samples')
-ax1.set_ylabel('Text Average', color='b')
-ax2.set_ylabel('Rating Average', color='r')
-plt.title('Text and Rating Averages')
-
-# Show the legend
-ax1.legend(loc='upper left')
-ax2.legend(loc='upper right')
-
-# Show the plot
-plt.show()
-
-vader_result_final['diff'] = vader_result_final['vader_rating_average'] - vader_result_final['vader_text_average']
-std_of_vader_diff = np.std(vader_result_final['diff'])
-correlation = vader_result_final['vader_text_average'].corr(vader_result_final['vader_rating_average'])
-
-#---------------------------------------------------------------------------
-#using roberta model
-from transformers import AutoTokenizer
-from transformers import AutoModelForSequenceClassification
-from scipy.special import softmax
-import torch
-import torch.nn as nn
 
 MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
@@ -98,117 +31,103 @@ def polarity_scores_roberta(txt):
         }
     return (scores_dict) 
 
-roberta_text_sample = pd.DataFrame(columns=column_names)
-roberta_rating_sample= pd.DataFrame(columns=column_names)
-roberta_sample_column = pd.DataFrame(columns=["roberta score"])
-np.random.seed(2002)
-for i in range(1):
-    sampled_element = df.sample(n, replace=True)
-    sampled_element_text = sampled_element['Text']
+
+n=1000
+sample = pd.DataFrame(columns=['Text', 'VADER_compound', 'roBERTa_compound', 'score'])
+sample['Text'] = df.Text.sample(n, replace=True, ignore_index=True, random_state=2002)
+
+#vader compounds to the sample
+vader_compound_scores = []
+for i in sample.Text:
+    vader_compound_scores.append(sia.polarity_scores(i)['compound'])
     
-    for j in sampled_element_text:
-        if len(j) > 512:
-            # Slice the text to the first 512 characters
-            j = j[:512]
-        try:
-            roberta_score = polarity_scores_roberta(j)
-            
-            roberta_sample_column.loc[len(roberta_sample_column)] = max(roberta_score.values())
-        except RuntimeError:
-            print(f"Broke for: {j}")
-    roberta_text_sample.loc[i] = roberta_sample_column["roberta score"].values
-    roberta_sample_column = roberta_sample_column.drop(roberta_sample_column.index, axis=0)
-    roberta_rating_sample.loc[i] = sampled_element['Score'].values
-    
-roberta_text_average = pd.DataFrame(np.mean(roberta_text_sample.iloc[0:k, :], axis=0), columns=["roberta_text_average"])
-roberta_rating_average = pd.DataFrame(np.mean(roberta_rating_sample.iloc[0:k, :], axis=0), columns=["roberta_rating_average"])
+sample['VADER_compound'] = vader_compound_scores
 
-# Merge the DataFrames
-roberta_result_final = roberta_text_average.merge(roberta_rating_average, how="left", left_index=True, right_index=True)
+#scores to the sample
+sample['score'] = df.Score.sample(n, replace=True, ignore_index=True, random_state=2002)
+
+#roberta compounds to the sample
+roberta_compound_scores = []
+for i in sample.Text:
+    i_scores = polarity_scores_roberta(i[0:514])
+    polarity_weights = torch.tensor([-1, 0, 1])
+    probs = torch.tensor([i_scores['roberta_neg'],
+                          i_scores['roberta_neu'],
+                          i_scores['roberta_pos']])
+    polarity = polarity_weights * probs
+    polarity = polarity.sum(dim=-1)
+    polarity_scaled = nn.Tanh()(polarity)
+    polarity_score = polarity_scaled.item()
+    roberta_compound_scores.append(polarity_score)
+
+sample['roBERTa_compound'] = roberta_compound_scores
+
+vader_corr = sample['score'].corr(sample['VADER_compound'])
+roberta_corr = sample['score'].corr(sample['roBERTa_compound'])
+print(vader_corr, roberta_corr)
+
+vader_mean = np.mean(sample['VADER_compound'])
+roberta_mean = np.mean(sample['roBERTa_compound'])
+score_mean = np.mean(sample['score'])
+print(vader_mean, roberta_mean, score_mean)
+
+#95% percent certainity for the entire dataset
+alpha = 1-0.95
+z = stats.norm.ppf(1-(alpha/2))
+SE_vader = stats.sem(sample['VADER_compound'])
+delta_vader = z*SE_vader
+SE_roberta = stats.sem(sample['roBERTa_compound'])
+delta_roberta = z*SE_roberta
+SE_score = stats.sem(sample['score'])
+delta_score = z*SE_score
+print([np.mean(sample['VADER_compound'])-delta_vader, np.mean(sample['VADER_compound'])+delta_vader])
+print([np.mean(sample['roBERTa_compound'])-delta_roberta, np.mean(sample['roBERTa_compound'])+delta_roberta])
+print([np.mean(sample['score'])-delta_score, np.mean(sample['score'])+delta_score])
+
+#-----------------------------------------------------------------------------
+#Misleading VADER model
+#Take the 30 data from the sample where the VADER and roBERTa compounds are the furthest from each other
+sample['diff'] = abs(sample['VADER_compound'] - sample['roBERTa_compound'])
+sample_sorted_diff = sample.sort_values(by='diff', ascending=False, ignore_index=True).head(30)
+sorted_corr_vader = sample_sorted_diff['score'].corr(sample_sorted_diff['VADER_compound'])
+sorted_corr_roberta = sample_sorted_diff['score'].corr(sample_sorted_diff['roBERTa_compound'])
+
+#in critical cases like this 30, the roBERTa model maintains a strong correlation with 'score'
+#while the VADER model fails to interpret the majority of these texts
+#it is due to the fact that VADER model only takes words, while roBERTa takes context as well
+#into account when calculating sentiment value
+print(sorted_corr_roberta, sorted_corr_vader)
 
 
+fig, ax1 = plt.subplots(figsize=(10, 6))
 
-rtest = "I am extremely sad"
-
-rtest_scores = polarity_scores_roberta(rtest)
-
-# Convert scores to tensors
-polarity_weights = torch.tensor([-1, 0, 1])
-probs = torch.tensor([rtest_scores['roberta_neg'], rtest_scores['roberta_neu'], rtest_scores['roberta_pos']])
-polarity = polarity_weights * probs
-polarity = polarity.sum(dim=-1)
-polarity_scaled = nn.Tanh()(polarity)
-polarity_score = polarity_scaled.item()  # Extracting the scalar value from the tensor
-
-print("RoBERTa Polarity Score:", polarity_score)
-print(sia.polarity_scores(rtest))
-
-print(rtest_scores['roberta_neg'] + rtest_scores['roberta_pos'])
-print(sia.polarity_scores(rtest))
-
-
-
-
-
-
-
-
-
-
-
-result = {}
-for i, row in tqdm(df.iterrows(), total=len(df)):
-    text = row["Text"]
-    myid = row["Id"]
-    result[myid] = sia.polarity_scores(text)
-    
-vader_result = pd.DataFrame(result).T
-vader_result = vader_result.reset_index().rename(columns={'index':'Id'})
-vader_result = vader_result.merge(df, how='left')
-
-#check if vader results are aligned with the ratings
-fig, axs = plt.subplots(1, 3, figsize=(15, 3))
-sns.barplot(data=vader_result, x='Score', y='pos', ax=axs[0])
-sns.barplot(data=vader_result, x='Score', y='neu', ax=axs[1])
-sns.barplot(data=vader_result, x='Score', y='neg', ax=axs[2])
-axs[0].set_title('Positive')
-axs[1].set_title('Neutral')
-axs[2].set_title('Negative')
+ax1.scatter(sample_sorted_diff.index, sample_sorted_diff['VADER_compound'], label='VADER_compound', color='blue', marker='o', edgecolor='black')
+ax1.scatter(sample_sorted_diff.index, sample_sorted_diff['roBERTa_compound'], label='roBERTa_compound', color='red', marker='x', edgecolor='black')
+ax1.set_ylabel('VADER and roBERTa compound scores')
+ax1.set_ylim(-1, 1.5)
+ax2 = ax1.twinx()
+ax2.scatter(sample_sorted_diff.index, sample_sorted_diff['score'], label='score', color='green', marker='s', edgecolor='black')
+ax2.set_ylabel('Score')
+ax2.set_ylim(0, 6)
+plt.xlabel('Index')
+plt.title('The top 30 largest difference between VADER roBERTa model compounds, compared to score')
+ax1.legend(loc='upper left')
+ax2.legend(loc='upper right')
+ax1.grid(True, linestyle='--', alpha=0.5)
+ax2.grid(True, linestyle='--', alpha=0.5)
 plt.tight_layout()
 plt.show()
 
-
-#doing the same analysis with the pretrained model roberta, which takes the relationship
-#between words into account
-
-
-   
-  
-res = {}  
-for i, row in tqdm(df.iterrows(), total=len(df)):
-    try:
-        text = row["Text"]
-        myid = row["Id"]
-        result_v = sia.polarity_scores(text)
-        result_r = polarity_scores_roberta(text)
-        both = {**result_v, **result_r}
-        res[myid] = both
-    except RuntimeError:
-        print(f"Broke for {myid}")
-                   
-
-#venni több mintát(szöveges értékelés compoundja) a sokaságból és abból becsülni a sokasági compound
-#értékére -> vader modell, vagy roberta modell csak meg kell nézni, hogy annak van egy compound-ja, 
-#vagy a vader honnan kapja meg a saját compoundját
-
-#probléma: nem normális eloszlású hanem exponenciális
-
-#df eredetin végigmenni random 50 elemmel és megnézni azoknak az eloszlását
-            
+    
 
 
 
-#vader modellel a compound értéket venni átlagnak -1 és 1 között, innen fogok becsülni
-#a sokasági átlagra
-#roberta modell esetében pedig a legnagyobb értéket venni átlagnak és onnan becsülni a 
-#sokasági átlagra
+
+
+
+
+
+
+
+
+
