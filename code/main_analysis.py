9566598495f8c336d98de94d3d5a2ea5bb8d1c4d
Epres Barnabás <barnabas.epres@gmail.com>
2024-04-23 19:05:06 +0200

    Code is finalised without unit tests

--- code/main_analysis.py
+++ code/main_analysis.py
@@ -8,28 +8,14 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from scipy.special import softmax
+from roberta_ps import *
 
 plt.style.use("ggplot")
 
-df = pd.read_csv('../sentiment_analysis_data/Reviews.csv')
+df = pd.read_csv('../Reviews.csv')
 
 sia = SentimentIntensityAnalyzer()
 
-MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
-model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-
-def polarity_scores_roberta(txt):
-    encoded_text = tokenizer(txt, return_tensors="pt")
-    output = model(**encoded_text)
-    scores = output[0][0].detach().numpy()
-    scores = softmax(scores)
-    scores_dict = {
-        'roberta_neg' : scores[0],
-        'roberta_neu' : scores[1],
-        'roberta_pos' : scores[2],
-        }
-    return (scores_dict) 
 
 
 n=1000
@@ -47,20 +33,7 @@ sample['VADER_compound'] = vader_compound_scores
 sample['score'] = df.Score.sample(n, replace=True, ignore_index=True, random_state=2002)
 
 #roberta compounds to the sample
-roberta_compound_scores = []
-for i in sample.Text:
-    i_scores = polarity_scores_roberta(i[0:514])
-    polarity_weights = torch.tensor([-1, 0, 1])
-    probs = torch.tensor([i_scores['roberta_neg'],
-                          i_scores['roberta_neu'],
-                          i_scores['roberta_pos']])
-    polarity = polarity_weights * probs
-    polarity = polarity.sum(dim=-1)
-    polarity_scaled = nn.Tanh()(polarity)
-    polarity_score = polarity_scaled.item()
-    roberta_compound_scores.append(polarity_score)
-
-sample['roBERTa_compound'] = roberta_compound_scores
+sample['roBERTa_compound'] = calc_roberta_compound(sample['Text'])
 
 vader_corr = sample['score'].corr(sample['VADER_compound'])
 roberta_corr = sample['score'].corr(sample['roBERTa_compound'])
@@ -80,9 +53,10 @@ SE_roberta = stats.sem(sample['roBERTa_compound'])
 delta_roberta = z*SE_roberta
 SE_score = stats.sem(sample['score'])
 delta_score = z*SE_score
-print([np.mean(sample['VADER_compound'])-delta_vader, np.mean(sample['VADER_compound'])+delta_vader])
-print([np.mean(sample['roBERTa_compound'])-delta_roberta, np.mean(sample['roBERTa_compound'])+delta_roberta])
-print([np.mean(sample['score'])-delta_score, np.mean(sample['score'])+delta_score])
+
+vader_ci = [np.mean(sample['VADER_compound'])-delta_vader, np.mean(sample['VADER_compound'])+delta_vader]
+roberta_ci = [np.mean(sample['roBERTa_compound'])-delta_roberta, np.mean(sample['roBERTa_compound'])+delta_roberta]
+score_ci = [np.mean(sample['score'])-delta_score, np.mean(sample['score'])+delta_score]
 
 #-----------------------------------------------------------------------------
 #Misleading VADER model
